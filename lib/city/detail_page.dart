53b03058dcf6c96a6b44f528254c4c42d63fff65
takashitosa <tosaki@nttdocomo.com>
2023-12-05 13:41:50 +0900

    Step6_2

--- lib/city/detail_page.dart
+++ lib/city/detail_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:resus_basic_app/env.dart';
+import 'package:http/http.dart' as http;
 
 class CityDetailPage extends StatefulWidget {
   const CityDetailPage({super.key, required this.city});
@@ -9,6 +11,30 @@ class CityDetailPage extends StatefulWidget {
 }
 
 class _CityDetailPageState extends State<CityDetailPage> {
+  late Future<String> _municipalitiresTaxesFuture;
+
+  @override
+  void initState(){
+    super.initState();
+    const host = 'opendata.resas-portal.go.jp';
+    const endpoint = '/api/v1/municipality/taxes/perYear';
+    final headers = {
+      'X-API-KEY': Env.resasApiKey,
+    };
+    final param = {
+      'prefCode': '14',
+      'cityCode': '14137',
+    };
+
+    _municipalitiresTaxesFuture = http
+    .get(
+      Uri.https(host,endpoint,param),
+      headers : headers,
+    )
+    .then((res) => res.body);
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
